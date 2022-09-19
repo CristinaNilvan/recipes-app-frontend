e44a8cfdb2d717ee2e483dff0b4375fd8d437ecd
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-09-19 11:53:10 +0300

    Add navbar component and update color palette

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeModule } from './recipes/recipe.module';
 import { IngredientModule } from './ingredients/ingredient.module';
+import { NavbarComponent } from './page-components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, RecipeModule, IngredientModule],
   providers: [],
   bootstrap: [AppComponent],
