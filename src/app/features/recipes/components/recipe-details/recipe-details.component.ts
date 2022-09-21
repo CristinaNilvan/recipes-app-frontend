1787e6891608ad2071e6c6d5e297f475841ce85d
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-09-21 21:29:27 +0300

    Refactor recipes components with angular material

--- src/app/features/recipes/components/recipe-details/recipe-details.component.ts
+++ src/app/features/recipes/components/recipe-details/recipe-details.component.ts
@@ -12,4 +12,32 @@ export class RecipeDetailsComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  getRecipeMealType() {
+    switch (this.recipe.mealType) {
+      case 0:
+        return 'Normal';
+      case 1:
+        return 'Vegetarian';
+      case 2:
+        return 'Vegan';
+      default:
+        return '';
+    }
+  }
+
+  getRecipeServingTime() {
+    switch (this.recipe.servingTime) {
+      case 0:
+        return 'Breakfast';
+      case 1:
+        return 'Lunch';
+      case 2:
+        return 'Dinner';
+      case 3:
+        return 'Others';
+      default:
+        return '';
+    }
+  }
 }
