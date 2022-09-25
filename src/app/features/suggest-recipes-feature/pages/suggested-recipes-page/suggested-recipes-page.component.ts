e3312370e477532bfcf6b3dd22f49fc099d3421e
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-09-25 21:32:38 +0300

    Add suggest recipes form

--- src/app/features/suggest-recipes-feature/pages/suggested-recipes-page/suggested-recipes-page.component.ts
+++ src/app/features/suggest-recipes-feature/pages/suggested-recipes-page/suggested-recipes-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Recipe } from 'src/app/core/models/recipe';
 import { RecipeService } from 'src/app/core/services/recipe.service';
 
@@ -8,15 +9,40 @@ import { RecipeService } from 'src/app/core/services/recipe.service';
   styleUrls: ['./suggested-recipes-page.component.css'],
 })
 export class SuggestedRecipesPageComponent implements OnInit {
-  suggestedRecipes: Recipe[] = [];
+  suggestRecipesForm!: FormGroup;
+  suggestedRecipes!: Recipe[];
   pageNumber: number = 1;
   pageSize: number = 5;
   ingredientName: string = 'Ing1';
   ingredientQuantity: number = 100;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit(): void {
+    this.suggestRecipesForm = this.formBuilder.group({
+      ingredientName: [
+        '',
+        [
+          Validators.required,
+          // Validators.pattern("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$"),
+          Validators.minLength(2),
+          Validators.maxLength(100),
+        ],
+      ],
+      ingredientQuantity: [
+        '',
+        [Validators.required, Validators.pattern('^-?[0-9]+(?:.[0-9]{1,2})?$')],
+      ],
+    });
+  }
+
+  onSubmit() {
+    this.ingredientName = this.ingredientNameFromForm;
+    this.ingredientQuantity = parseFloat(this.ingredientQuantityFromForm);
+
     this.recipeService
       .getSuggestedRecipes(
         this.pageNumber,
@@ -25,6 +51,10 @@ export class SuggestedRecipesPageComponent implements OnInit {
         this.ingredientQuantity
       )
       .subscribe((recipes) => (this.suggestedRecipes = recipes));
+  }
+
+  onClear() {
+    this.suggestRecipesForm.reset();
   }
 
   onScroll() {
@@ -36,5 +66,13 @@ export class SuggestedRecipesPageComponent implements OnInit {
         this.ingredientQuantity
       )
       .subscribe((recipes) => this.suggestedRecipes.push(...recipes));
+  }
+
+  get ingredientNameFromForm() {
+    return this.suggestRecipesForm.get('ingredientName')?.value;
+  }
+
+  get ingredientQuantityFromForm(): string {
+    return this.suggestRecipesForm.get('ingredientQuantity')?.value;
   }
 }
