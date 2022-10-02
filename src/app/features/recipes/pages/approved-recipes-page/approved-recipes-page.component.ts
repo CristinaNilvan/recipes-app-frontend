21cb6f0fed8fde9c884cd604681d773c92748091
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-10-02 10:37:15 +0300

    Delete unused recipe components and refactor all-recipes-page

--- src/app/features/recipes/pages/approved-recipes-page/approved-recipes-page.component.ts
+++ src/app/features/recipes/pages/approved-recipes-page/approved-recipes-page.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from 'src/app/core/models/get-models/recipe';
 import { RecipeService } from '../../../../core/services/recipe.service';
@@ -11,18 +12,37 @@ export class ApprovedRecipesPageComponent implements OnInit {
   approvedRecipes: Recipe[] = [];
   pageNumber: number = 1;
   pageSize: number = 5;
+  responseMessage: string = '';
 
   constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
+    this.responseMessage = '';
+
     this.recipeService
       .getApprovedRecipes(this.pageNumber, this.pageSize)
-      .subscribe((recipes) => (this.approvedRecipes = recipes));
+      .subscribe({
+        next: (recipes) => (this.approvedRecipes = recipes),
+        error: (error: HttpErrorResponse) => {
+          if (error.status === 404) {
+            this.responseMessage = 'Recipes not found!';
+          }
+        },
+      });
   }
 
   onScroll() {
+    this.responseMessage = '';
+
     this.recipeService
       .getApprovedRecipes(++this.pageNumber, this.pageSize)
-      .subscribe((recipes) => this.approvedRecipes.push(...recipes));
+      .subscribe({
+        next: (recipes) => this.approvedRecipes.push(...recipes),
+        error: (error: HttpErrorResponse) => {
+          if (error.status === 404) {
+            this.responseMessage = 'No more recipes!';
+          }
+        },
+      });
   }
 }
--- src/app/features/recipes/recipe.module.ts
+++ src/app/features/recipes/recipe.module.ts
@@ -3,11 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
 import { RecipeDetailsCardComponent } from './components/recipe-details-card/recipe-details-card.component';
 import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
-import { AllRecipesPageComponent } from './pages/all-recipes-page/all-recipes-page.component';
 import { CoreModule } from 'src/app/core/core.module';
-import { RecipeDetailsPageComponent } from './pages/recipe-details-page/recipe-details-page.component';
 import { ApprovedRecipesPageComponent } from './pages/approved-recipes-page/approved-recipes-page.component';
-import { UnapprovedRecipesPageComponent } from './pages/unapproved-recipes-page/unapproved-recipes-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { IngredientModule } from '../ingredients/ingredient.module';
 import { CreateRecipeStepperComponent } from './components/create-recipe-stepper/create-recipe-stepper.component';
@@ -25,10 +22,7 @@ import { UpdateRecipeDetailsComponent } from './components/update-recipe-details
     RecipesListComponent,
     RecipeDetailsCardComponent,
     RecipeDetailsComponent,
-    AllRecipesPageComponent,
-    RecipeDetailsPageComponent,
     ApprovedRecipesPageComponent,
-    UnapprovedRecipesPageComponent,
     CreateRecipeStepperComponent,
     CreateRecipeIngredientComponent,
     CreateRecipeIngredientListComponent,
@@ -44,10 +38,7 @@ import { UpdateRecipeDetailsComponent } from './components/update-recipe-details
     RecipeDetailsCardComponent,
     RecipesListComponent,
     CreateRecipeStepperComponent,
-    AllRecipesPageComponent,
     ApprovedRecipesPageComponent,
-    UnapprovedRecipesPageComponent,
-    RecipeDetailsPageComponent,
     CrudRecipePageComponent,
     GetRecipeComponent,
     UpdateRecipeComponent,
