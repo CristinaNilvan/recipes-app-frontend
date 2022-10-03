a6c04dfb85a9fd6c4207ac67a5174388e53de43a
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-10-03 11:58:09 +0300

    Move components

--- src/app/features/admin-feature/admin.module.ts
+++ src/app/features/admin-feature/admin.module.ts
@@ -6,12 +6,12 @@ import { CoreModule } from '@angular/flex-layout';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { IngredientsListAdminComponent } from './components/ingredients-list-admin/ingredients-list-admin.component';
 import { RecipesListAdminComponent } from './components/recipes-list-admin/recipes-list-admin.component';
-import { AllIngredientsAdminComponent } from './pages/all-ingredients-admin/all-ingredients-admin.component';
-import { ApprovedIngredientsAdminComponent } from './pages/approved-ingredients-admin/approved-ingredients-admin.component';
-import { UnapprovedIngredientsAdminComponent } from './pages/unapproved-ingredients-admin/unapproved-ingredients-admin.component';
-import { AllRecipesAdminComponent } from './pages/all-recipes-admin/all-recipes-admin.component';
-import { ApprovedRecipesAdminComponent } from './pages/approved-recipes-admin/approved-recipes-admin.component';
-import { UnapprovedRecipesAdminComponent } from './pages/unapproved-recipes-admin/unapproved-recipes-admin.component';
+import { AllIngredientsAdminComponent } from './components/all-ingredients-admin/all-ingredients-admin.component';
+import { ApprovedIngredientsAdminComponent } from './components/approved-ingredients-admin/approved-ingredients-admin.component';
+import { UnapprovedIngredientsAdminComponent } from './components/unapproved-ingredients-admin/unapproved-ingredients-admin.component';
+import { AllRecipesAdminComponent } from './components/all-recipes-admin/all-recipes-admin.component';
+import { ApprovedRecipesAdminComponent } from './components/approved-recipes-admin/approved-recipes-admin.component';
+import { UnapprovedRecipesAdminComponent } from './components/unapproved-recipes-admin/unapproved-recipes-admin.component';
 import { AdminFeaturesComponent } from './pages/admin-features/admin-features.component';
 
 @NgModule({
