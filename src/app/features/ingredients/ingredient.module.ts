fe947e1b4af3ff2f05d88f9d223522e3fae1cd9b
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-10-04 17:49:30 +0300

    Finalize recipes and ingredients pages

--- src/app/features/ingredients/ingredient.module.ts
+++ src/app/features/ingredients/ingredient.module.ts
@@ -8,7 +8,6 @@ import { ApprovedIngredientsPageComponent } from './pages/approved-ingredients-p
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CreateIngredientComponent } from './components/create-ingredient/create-ingredient.component';
 import { CrudIngredientPageComponent } from './pages/crud-ingredient-page/crud-ingredient-page.component';
-import { IngredientRoutingModule } from './ingredient-routing.module';
 import { GetIngredientComponent } from './components/get-ingredient/get-ingredient.component';
 import { DeleteIngredientComponent } from './components/delete-ingredient/delete-ingredient.component';
 import { UpdateIngredientComponent } from './components/update-ingredient/update-ingredient.component';
@@ -27,7 +26,7 @@ import { UpdateIngredientDetailsComponent } from './components/update-ingredient
     UpdateIngredientComponent,
     UpdateIngredientDetailsComponent,
   ],
-  imports: [CommonModule, IngredientRoutingModule, CoreModule, SharedModule],
+  imports: [CommonModule, CoreModule, SharedModule],
   exports: [
     ApprovedIngredientsPageComponent,
     CrudIngredientPageComponent,
