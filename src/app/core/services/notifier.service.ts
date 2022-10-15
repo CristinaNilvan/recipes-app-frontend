ed05c4a8f6ccfe9455550ba8c0066c5de7c250d1
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-10-15 14:16:54 +0300

    Add notifier service

---
+++ src/app/core/services/notifier.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotifierService {
+  constructor(private snackBar: MatSnackBar) {}
+
+  showNotification(displayMessage: string) {
+    this.snackBar.open(displayMessage, 'Close', {
+      duration: 5000,
+      panelClass: ['snackbar'],
+    });
+  }
+}
